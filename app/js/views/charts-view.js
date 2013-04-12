32ab434b25085beca1cad4062a1115072965f041
gho897 <gho897@LXPD-J0QM2Q1.prod-am.ameritrade.com>
2013-04-12 09:54:19 -0400

    Updating Charts

--- app/js/views/charts-view.js
+++ app/js/views/charts-view.js
@@ -1,50 +1,71 @@
 //Chart View
 var app = app || {};
 var ChartView = Backbone.View.extend({
-    el: '.page',      
+    el: '.page',
     initialize: function() {
         console.log("Chart initialize " + app.userProfileModel);
-        this.chartModel = new ChartModel();
-        _.bindAll(this,'render');
-      
+        this.timeinterval;      
+        this.periodType;               
+        this.endDate = $.datepicker.formatDate('yymmdd', new Date());         
+        this.chartModel = new ChartModel();        
+        _.bindAll(this, 'render');
+
     },
     render: function() {
-       console.log('render quote detail view');
-       var template = _.template($('#chart_template').html(),{});
-       this.$el.html(template);    
-       if(this.chartModel.get('symbol') !=='')
+        var template = _.template($('#chart_template').html(), {});
+        this.$el.html(template);
+        if (this.chartModel.get('symbol') !== '')
             $('#chartSymbol').val(this.chartModel.get('symbol'));
-       this.drawCharts();
-       
+        this.drawCharts();
+
     },
-    events:{
-            'click #goButton':'getPriceHistory'
+    events: {
+        'click #goButton': 'getPriceHistory'
     },
     getPriceHistory: function()
     {
-        var symbol = $('#chartSymbol').val();      
-        this.chartModel.set({symbol:symbol});
+        var symbol = $('#chartSymbol').val();
+        this.chartModel.set({symbol: symbol});
         var sessionToken = app.userProfileModel.get('session-id');
-
         var oReq = new XMLHttpRequest();
         var url = 'https://apista.tdameritrade.com/apps/100/PriceHistory?jsessionid=' + sessionToken +
-                '&requestidentifiertype=SYMBOL&requestvalue='+ symbol + '&source=TAG' +
+                '&requestidentifiertype=SYMBOL&requestvalue=' + symbol + '&source=TAG' +
                 '&intervaltype=DAILY&periodtype=YEAR' +
-                '&intervalduration=1&enddate=20130409&period=3' +
+                '&intervalduration=1&enddate=' +this.endDate+'&period=3' +
                 '&extended=false';
         oReq.open('GET', url, true);
         oReq.responseType = 'arraybuffer';
-        oReq.onload = function(oEvent) {
-            var arrayBuffer = oReq.response; // Note: not oReq.responseText
+        oReq.onload = function(oEvent){
+             var arrayBuffer = oReq.response; 
+             app.chartView.priceHistoryResponse(arrayBuffer,false);
+        };
+        oReq.send(null);
+        
+        var dailyReq = new XMLHttpRequest();
+        var url = 'https://apista.tdameritrade.com/apps/100/PriceHistory?jsessionid=' + sessionToken +
+                '&requestidentifiertype=SYMBOL&requestvalue=' + symbol + '&source=TAG' +
+                '&intervaltype=MINUTE&periodtype=DAY' +
+                '&intervalduration=5&enddate=' +this.endDate+'&period=1' +
+                '&extended=false';
+        dailyReq.open('GET', url, true);
+        dailyReq.responseType = 'arraybuffer';
+        dailyReq.onload = function(oEvent){
+             var arrayBuffer = dailyReq.response; 
+             app.chartView.priceHistoryResponse(arrayBuffer,true);
+        };
+        dailyReq.send(null);
+
+    },
+    priceHistoryResponse:function(arrayBuffer,dailyChart) {          
             if (arrayBuffer) {
                 var byteArray = new Uint8Array(arrayBuffer);
                 var view = new jDataView(byteArray);
                 var symCount = view.getInt32();
-                var symStr = view.getUTF8String(6,symbol.length);
+                var symStr = view.getUTF8String(6, this.chartModel.get('symbol').length);
                 var errorCode = view.getInt8();
                 var barCount = view.getInt32();
                 var ohlc = [],
-                volume = [];
+                        volume = [];
                 for (var i = 0; i < barCount; i++)
                 {
                     var close = view.getFloat32();
@@ -68,63 +89,104 @@ var ChartView = Backbone.View.extend({
                         symVolume // the volume
                     ]);
                 }
-                app.chartView.chartModel.set({ohlc: ohlc});
-                app.chartView.chartModel.set({volume: volume});
-                app.chartView.drawCharts();
+                if(dailyChart){
+                    this.chartModel.set({dailyohlc: ohlc});
+                    this.chartModel.set({dailyvolume: volume});                    
+                }else{
+                    this.chartModel.set({ohlc: ohlc});
+                    this.chartModel.set({volume: volume});
+                    this.drawCharts();
+                }
+                
+               
             }
-        };
-        oReq.send(null);
-
-    },            
-    drawCharts: function(){
-        var groupingUnits = [[
-                        'week', // unit name
-                        [1]                             // allowed multiples
-                    ], [
-                        'month',
-                        [1, 2, 3, 4, 6]
-                    ]];
-
-              
-                $('#chartPage').highcharts('StockChart', {
-                    rangeSelector: {
-                        selected: 1
-                    },
+        },        
+    drawCharts: function() {        
+       var dailyseries= [{
+                    type: 'candlestick',
+                    name: this.chartModel.get('symbol'),
+                    data: this.chartModel.get('dailyohlc')                  
+                }, {
+                    type: 'column',
+                    name: 'Volume',
+                    data: this.chartModel.get('dailyvolume'),
+                    yAxis: 1                    
+                }];
+        var series = [{
+                    type: 'candlestick',
+                    name: this.chartModel.get('symbol'),
+                    data: this.chartModel.get('ohlc')                  
+                }, {
+                    type: 'column',
+                    name: 'Volume',
+                    data: this.chartModel.get('volume'),
+                    yAxis: 1                    
+                }];
+        var chart = new Highcharts.StockChart({
+            chart: {
+                renderTo: 'chartPage',
+                events: {
+                    load: function() {
+                        var chart = this,                              
+                               buttons = chart.rangeSelector.buttons;                       
+                        buttons[0].on('click', function(e) {
+                        chart.series =  dailyseries;
+                        });
+                    }
+                }
+            },
+            rangeSelector: {
+             buttons: [{
+                type: 'hour',
+                count: 1,
+                text: '1h'
+            }, 
+                 {
+                type: 'day',
+                count: 1,
+                text: '1d'
+            }, {
+                type: 'month',
+                count: 1,
+                text: '1m'
+            }, {
+                type: 'month',
+                count: 3,
+                text: '3m'
+            }, {
+                type: 'month',
+                count: 6,
+                text: '6m'
+            }, {
+                type: 'year',
+                count: 1,
+                text: '1y'
+            }, {
+                type: 'all',
+                text: 'All'
+            }],
+                selected: 2
+            },
+            title: {
+                text: this.chartModel.get('symbol') + ' Historical'
+            },
+            yAxis: [{
                     title: {
-                        text: this.chartModel.get('symbol') + ' Historical'
+                        text: 'OHLC'
                     },
-                    yAxis: [{
-                            title: {
-                                text: 'OHLC'
-                            },
-                            height: 200,
-                            lineWidth: 2
-                        }, {
-                            title: {
-                                text: 'Volume'
-                            },
-                            top: 300,
-                            height: 100,
-                            offset: 0,
-                            lineWidth: 2
-                        }],
-                    series: [{
-                            type: 'candlestick',
-                            name: this.chartModel.get('symbol'),
-                            data: this.chartModel.get('ohlc'),
-                            dataGrouping: {
-                                units: groupingUnits
-                            }
-                        }, {
-                            type: 'column',
-                            name: 'Volume',
-                            data: this.chartModel.get('volume'),
-                            yAxis: 1,
-                            dataGrouping: {
-                                units: groupingUnits
-                            }
-                        }]
-                });
+                    height: 200,
+                    lineWidth: 2
+                }, {
+                    title: {
+                        text: 'Volume'
+                    },
+                    top: 300,
+                    height: 100,
+                    offset: 0,
+                    lineWidth: 2
+                }],
+            series:series
+        });
     }
 
 });
